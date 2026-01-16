e1383707209b8daddc09e4e30415498fc9aa4075
naveenpatel111 <naveenpatel972@gmail.com>
2026-01-16 12:00:41 +0530

    Footer Changed

--- src/components/FooterComponent/Footer.js
+++ src/components/FooterComponent/Footer.js
@@ -1,13 +1,21 @@
+
+import './Footer.css'
+
 export default function Footer() {
   return (
-    <footer>
-      <div className="container">
-        <p>
-          © 2048 Villa Agency. Design by{' '}
-          <a href="https://templatemo.com" target="_blank" rel="noreferrer">
-            TemplateMo
-          </a>
+    <footer className="footer">
+      <div className="footer-container">
+
+        <p className="footer-text">
+          © {new Date().getFullYear()} Reliable Movers & Packers. All Rights Reserved.
         </p>
+
+        <div className="footer-links">
+          <a href="/about">About</a>
+          <a href="/service">Services</a>
+          <a href="/contact">Contact</a>
+        </div>
+
       </div>
     </footer>
   );
